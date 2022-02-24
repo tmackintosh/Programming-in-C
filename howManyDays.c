626469791d3749dcad21ff2868bbf29c9b8d3c89
Tom Mackintosh <tom.mackintosh2001@live.co.uk>
2022-02-24 19:41:57 +0000

    Iterate through all the months

--- howManyDays.c
+++ howManyDays.c
@@ -10,89 +10,6 @@ typedef struct {
     int daysFromStart;
 } Date;
 
-Date advanceMonth (Date currentDate) {
-    int increments[12] = {
-        31,
-        28,
-        31,
-        30,
-        31,
-        30,
-        31,
-        31,
-        30,
-        31,
-        30,
-        31
-    };
-
-    currentDate.daysFromStart += increments[currentDate.month - 1];
-    
-    // Check for leap-year anomalies
-    if (currentDate.month == 2) {
-        if (currentDate.year % 4 == 0 && currentDate.year % 400 != 0) {
-            currentDate.daysFromStart += 1;
-        }
-    }
-
-    currentDate.month += 1;
-
-    // In case the year has incremented.
-    if (currentDate.month > 12) {
-        currentDate.month = (currentDate.month % 12);
-
-        // Edge case that currentDate.month may have been 24 or
-        // any other multiple of 12 other than 12.
-        if (currentDate.month == 0) {
-            currentDate.year += currentDate.month / 12;
-            currentDate.month = 12;
-        }
-        else {
-            currentDate.year += 1;
-        }
-    }
-
-    return currentDate;
-}
-
-int getWeekday(Date currentDate) {
-    // As 1/1/1801 is a Sunday, we remove 1 to adjust
-    // for the weekday.
-    return (currentDate.daysFromStart - 1) % 7;
-}
-
-void displayCurrentDate (Date currentDate) {
-    char weekdays[7][10] = {
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-        "Sunday"
-    };
-
-    char months[12][10] = {
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
-    };
-
-    printf("%s ", weekdays[getWeekday(currentDate)]);
-    printf("%d ", currentDate.day);
-    printf("%s ", months[currentDate.month - 1]);
-    printf("%d\n", currentDate.year);
-}
-
 int getDaysInMonth (int month, int year) {
     switch (month) {
         case 1:
@@ -176,14 +93,74 @@ int calculateDaysFromStart (Date currentDate) {
     return daysFromStart;
 }
 
+Date advanceMonth (Date currentDate) {
+    currentDate.day += getDaysInMonth(currentDate.month, currentDate.year);
+
+    // Recall getDaysInMonth function to recalculate days in the current month
+    // to maintain consistency.
+    while (currentDate.day > getDaysInMonth(currentDate.month, currentDate.year)) {
+        currentDate.day -= getDaysInMonth(currentDate.month, currentDate.year);
+        currentDate.month += 1;
+
+        while (currentDate.month > 12) {
+            currentDate.month -= 12;
+            currentDate.year += 1;
+        }
+    }
+
+    currentDate.daysFromStart = calculateDaysFromStart(currentDate);
+    return currentDate;
+}
+
+int getWeekday(Date currentDate) {
+    // As 1/1/1801 is a Sunday, we remove 1 to adjust
+    // for the weekday.
+    return (currentDate.daysFromStart - 1) % 7;
+}
+
+void displayCurrentDate (Date currentDate) {
+    char weekdays[7][10] = {
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+    };
+
+    char months[12][10] = {
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    };
+
+    printf("%s ", weekdays[getWeekday(currentDate)]);
+    printf("%d ", currentDate.day);
+    printf("%s ", months[currentDate.month - 1]);
+    printf("%d\n", currentDate.year);
+}
+
 int howManyDays() {
     Date currentDate;
-    currentDate.year = 2002;
-    currentDate.month = 3;
-    currentDate.day = 24;
+    currentDate.year = 1801;
+    currentDate.month = 1;
+    currentDate.day = 5;
     currentDate.daysFromStart = calculateDaysFromStart(currentDate);
 
-    displayCurrentDate(currentDate);
+    while(currentDate.year < 2001) {
+        displayCurrentDate(currentDate);
+        currentDate = advanceMonth(currentDate);
+    }
     
     return 0;
 }
