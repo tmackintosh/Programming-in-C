2ccfb1f39a2b8a5fa817df4f714d00fbc4d1d2ce
Tom Mackintosh <tom.mackintosh2001@live.co.uk>
2022-02-24 19:26:37 +0000

    Dynamically calculate day numbers

--- howManyDays.c
+++ howManyDays.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 // Returns how many Fridays fell on the fifth of the month during the nineteenth and twentieth centuries (1 Jan 1801 to 31 Dec 2000) given the dates in the question text
 typedef struct {
@@ -55,7 +56,9 @@ Date advanceMonth (Date currentDate) {
 }
 
 int getWeekday(Date currentDate) {
-    return (currentDate.daysFromStart - 2) % 7;
+    // As 1/1/1801 is a Sunday, we remove 1 to adjust
+    // for the weekday.
+    return (currentDate.daysFromStart - 1) % 7;
 }
 
 void displayCurrentDate (Date currentDate) {
@@ -90,36 +93,98 @@ void displayCurrentDate (Date currentDate) {
     printf("%d\n", currentDate.year);
 }
 
+int getDaysInMonth (int month, int year) {
+    switch (month) {
+        case 1:
+            return 31;
+            break;
+
+        case 2:
+            if (year % 4 == 0 && (!year % 100 == 0 || year % 400 == 0)) {
+                return 29;
+            }
+            else {
+                return 28;
+            }
+
+            break;
+
+        case 3:
+            return 31;
+            break;
+
+        case 4:
+            return 30;
+            break;
+
+        case 5:
+            return 31;
+            break;
+
+        case 6:
+            return 30;
+            break;
+
+        case 7:
+            return 31;
+            break;
+
+        case 8:
+            return 31;
+            break;
+
+        case 9:
+            return 30;
+            break;
+
+        case 10:
+            return 31;
+            break;
+
+        case 11:
+            return 30;
+            break;
+
+        case 12:
+            return 31;
+            break;
+    }
+
+    return -1;
+}
+
 int calculateDaysFromStart (Date currentDate) {
     int daysFromStart = 0;
 
     int deltaYear = currentDate.year - 1801;
     daysFromStart += (365 * deltaYear);
 
-    int leapYears = (deltaYear + 2) / 4;
+    int leapYears = (deltaYear) / 4;
     daysFromStart += leapYears;
 
     if (currentDate.year >= 2000) {
         daysFromStart -= 1;
     }
 
+    while (currentDate.month > 1) {
+        daysFromStart += getDaysInMonth(currentDate.month - 1, currentDate.year);
+        currentDate.month -= 1;
+    }
+
+    daysFromStart += currentDate.day - 1;
+
     return daysFromStart;
 }
 
 int howManyDays() {
     Date currentDate;
-    currentDate.year = 1801;
+    currentDate.year = 2002;
     currentDate.month = 3;
     currentDate.day = 24;
-    currentDate.daysFromStart = 30 + 28 + 24;
+    currentDate.daysFromStart = calculateDaysFromStart(currentDate);
 
     displayCurrentDate(currentDate);
-
-    while (currentDate.year != 2004) {
-        currentDate = advanceMonth(currentDate);
-        displayCurrentDate(currentDate);
-    }
-
+    
     return 0;
 }
 
