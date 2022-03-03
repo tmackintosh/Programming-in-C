d328f04472c3b781901edfe9ee24901879389bb2
Tom Mackintosh <tom.mackintosh2001@live.co.uk>
2022-03-03 17:19:35 +0000

    Find 2 numbers successfully

--- removeDigit.c
+++ removeDigit.c
@@ -1,18 +1,57 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+int contains(int value, int digit) {
+    int order_of_magnitude = 0;
+
+    while (value != 0) {
+        int num = value % 10;
+
+        if (num == digit) {
+            return true;
+        }
+
+        value /= 10;
+        order_of_magnitude += 1;
+    }
+
+    return false;
+}
 
 void removeDigit(int x, int n, int* a_ptr, int* b_ptr){
-    *a_ptr = 10;
-    *b_ptr = 10;
+    int a, b;
+
+    if (n % 2 == 0) {
+        a = (n / 2) - 1;
+        b = (n / 2) + 1;
+    } else {
+        a = (n / 2);
+        b = (n / 2) + 1;
+    }
+
+    while (true) {
+        if (!contains(a, x) && !contains(b, x)) {
+            *a_ptr = a;
+            *b_ptr = b;
+
+            return;
+        }
+
+        a -= 1;
+        b += 1;
+    }
 }
 
 int main() {
-    int* a_ptr,* b_ptr;
-    int a = 10;
-    int b = 20;
+    int* a_ptr,* b_ptr, a, b;
 
     a_ptr = &a;
     b_ptr = &b;
 
-    removeDigit(10, 10, a_ptr, b_ptr);
+    removeDigit(3, 202, a_ptr, b_ptr);
+
+    printf("A: %d\n", *a_ptr);
+    printf("B: %d\n", *b_ptr);
+
     return 0;
 }
