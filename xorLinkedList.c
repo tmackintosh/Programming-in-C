63c154c2c4fdf0523df75867bf3f6b805e4d01b2
Tom Mackintosh <tom.mackintosh2001@live.co.uk>
2022-03-14 14:00:08 +0000

    Print list

--- xorLinkedList.c
+++ xorLinkedList.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 typedef struct node {
     char* name;
@@ -13,24 +14,43 @@ Node* calculate_xor_value(Node* before, Node* after) {
 
 Node* make_new_node(const char *string) {
     Node *newNode = (Node*) malloc(sizeof(Node));
-    strcpy(newNode -> name, string);
+    newNode -> name = (char *)string;
+    newNode -> xor_value = NULL;
     
     return newNode;
 }
 
+void print_list(Node* head) {
+    Node *prev = NULL;
+    Node *curr = head;
+    Node* next;
+
+    while (curr != NULL) {
+        printf("%s -> ", curr -> name);
+        next = calculate_xor_value(prev, curr -> xor_value);
+        prev = curr;
+        curr = next;
+    }
+
+    printf("NULL\n");
+}
+
 // Inserts the string at the beginning of the XOR linked list.
 void insert_string(Node** head, const char* newObj) {
+    if (strlen(newObj) > 64) {
+        printf("Can't insert more than 64 characters in string");
+    }
+
     Node *newNode = make_new_node(newObj);
     newNode -> xor_value = calculate_xor_value(NULL, *head);
 
     // We should only look for the next node if the list isn't empty
-    if (head != NULL) {
-        Node *nextNode = calculate_xor_value(NULL, *head);
+    if (*head != NULL) {
+        Node *nextNode = calculate_xor_value(NULL, (*head) -> xor_value);
         (*head) -> xor_value = calculate_xor_value(newNode, nextNode);
     }
 
     *head = newNode;
-}
 
 // If possible, inserts before the string "before" and returns true. Returns false if not possible (e.g., the before string is not in the list).
 int insert_before(Node** head, const char* before, const char* newObj) {
@@ -63,5 +83,8 @@ void free_all(Node** head) {
 }
 
 int main () {
-    return 0;
+    Node *head = NULL;
+    insert_string(&head, "Alpha");
+    insert_string(&head, "Bravo");
+    print_list(head);
 }
