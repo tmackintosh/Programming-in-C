05db1973a7ff5da3dcf96bd9a45d143dd985ffaf
Tom Mackintosh <tom.mackintosh2001@live.co.uk>
2022-03-15 13:37:04 +0000

    Remove after and free remaining

--- xorLinkedList.c
+++ xorLinkedList.c
@@ -137,14 +137,13 @@ int remove_string(Node** head, char* result) {
 
 // If possible, removes the string after the string "after" and fills in the result buffer with its value. If successful return true, otherwise returns false
 int remove_after(Node** head, const char *after, char *result) {
-    Node *previousPreviousNode = NULL;
     Node *previousNode = NULL;
     Node *currentNode = *head;
     Node *nextNode = calculate_xor_value(NULL, currentNode -> xor_value);
+    Node *nextNextNode = NULL;
 
     while (currentNode != NULL) {
         nextNode = calculate_xor_value(previousNode, currentNode -> xor_value);
-        previousPreviousNode = previousNode;
         previousNode = currentNode;
         currentNode = nextNode;
 
@@ -152,17 +151,24 @@ int remove_after(Node** head, const char *after, char *result) {
             return false;
         }
 
-        if (previousNode -> name == after) {
-            strcpy(result, currentNode -> name);
+        if (currentNode -> name == after) {
             nextNode = calculate_xor_value(previousNode, currentNode -> xor_value);
-            previousNode -> xor_value = calculate_xor_value(previousPreviousNode, nextNode);
-            
-            if (nextNode != NULL) {
-                Node *nextNextNode = calculate_xor_value(currentNode, nextNode -> xor_value);
-                nextNode -> xor_value = calculate_xor_value(previousNode, nextNextNode);
+
+            if (nextNode == NULL) {
+                return false;
             }
 
-            free(currentNode);
+            nextNextNode = calculate_xor_value(currentNode, nextNode -> xor_value);
+
+            strcpy(result, nextNode -> name);
+
+            currentNode -> xor_value = calculate_xor_value(previousNode, nextNextNode);
+
+            if (nextNextNode != NULL) {
+                Node *nextNextNextNode = calculate_xor_value(nextNode, nextNextNode -> xor_value);
+                nextNextNode -> xor_value = calculate_xor_value(nextNode, nextNextNextNode);
+            }
+
             return true;
         }
     }
@@ -172,12 +178,50 @@ int remove_after(Node** head, const char *after, char *result) {
 
 // If possible, removes the string before the string "before" and fills in the result buffer with its value. If successful return true, otherwise returns false.
 int remove_before(Node** head, const char *before, char *result) {
-    return -1;
+    Node *previousPreviousPreviousNode = NULL;
+    Node *previousPreviousNode = NULL;
+    Node *previousNode = NULL;
+    Node *currentNode = *head;
+    Node *nextNode = calculate_xor_value(NULL, currentNode -> xor_value);
+
+    while (currentNode != NULL) {
+        nextNode = calculate_xor_value(previousNode, currentNode -> xor_value);
+        previousPreviousPreviousNode = previousPreviousNode;
+        previousPreviousNode = previousNode;
+        previousNode = currentNode;
+        currentNode = nextNode;
+
+        if (currentNode == NULL) {
+            return false;
+        }
+
+        if (currentNode -> name == before) {
+            if (previousNode == *head) {
+                return remove_string(head, result);
+            }
+
+            strcpy(result, previousNode -> name);
+            nextNode = calculate_xor_value(previousNode, currentNode -> xor_value);
+            previousPreviousNode -> xor_value = calculate_xor_value(previousPreviousPreviousNode, currentNode);
+            currentNode -> xor_value = calculate_xor_value(previousPreviousNode, nextNode);
+
+            return true;
+        }
+    }
+
+    return false;
 }
 
 // Removes all nodes and releases any memory allocated to the linked list
 void free_all(Node** head) {
-    
+    Node *currentNode = *head;
+    Node *nextNode = calculate_xor_value(NULL, currentNode -> xor_value);
+
+    while (nextNode != NULL) {
+        nextNode = calculate_xor_value(currentNode, nextNode -> xor_value);
+        free(currentNode);
+        currentNode = nextNode;
+    }
 }
 
 int main () {
@@ -186,16 +230,21 @@ int main () {
     insert_string(&head, "Alpha");
     insert_after(&head, "Charlie", "Delta");
     insert_after(&head, "Alpha", "Bravo");
-    print_list(head);
 
     char result[1024];
     remove_string(&head, result);
     remove_after(&head, "Charlie", result);
     insert_before(&head, "Bravo", "Alpha");
     remove_after(&head, "Bravo", result);
+    remove_before(&head, "Bravo", result);
+    insert_string(&head, "Alpha");
+    insert_after(&head, "Bravo", "Charlie");
+    remove_before(&head, "Charlie", result);
 
     printf("%s\n", result);
     print_list(head);
+
+    free_all(&head);
 
     return 0;
 }
